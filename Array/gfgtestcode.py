c59223d24bb1ba1d3bfeb6f4e6d36bd2563d5fc3
suraj <surajkolekar123@gmail.com>
2021-02-20 21:01:21 +0530

    added fullstop

--- Array/gfgtestcode.py
+++ Array/gfgtestcode.py
@@ -11,5 +11,5 @@ print(acc.id)   #output==> 111
 	Explanation: Instantiation of the class “Acc” automatically calls the method __init__ and passes the object 
 				as the self parameter. 111 is assigned to data attribute of the object called id.
 				The value “555” is not retained in the object as it is not assigned to a data attribute 
-				of the class/object. So, the output of the program is “111”
+				of the class/object. So, the output of the program is “111”.
 '''
